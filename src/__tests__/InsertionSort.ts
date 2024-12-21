918b4255a4e22aea459240feb55e7c9340d3fd6d
Philip Vallone <pv@philips.macbook.pro.lan>
2024-12-21 07:35:32 -0600

    Add more insertion sort tests

--- src/__tests__/InsertionSort.ts
+++ src/__tests__/InsertionSort.ts
@@ -6,4 +6,10 @@ test("insertion-sort", function () {
     // where is my debugger
     insertion_sort(arr);
     expect(arr).toEqual([3, 4, 7, 9, 42, 69, 420]);
+    const arr2 = [1, 2, 3, 4];
+    insertion_sort(arr2);
+    expect(arr2).toEqual([1, 2, 3, 4]);
+    const arr3 = [4, 3, 2, 1];
+    insertion_sort(arr3);
+    expect(arr3).toEqual([1, 2, 3, 4]);
 });
